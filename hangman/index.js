855129bdacd68be485644a3dc67042a75ed26c00
Veronica <veronica.gudova@gmail.com>
2021-02-17 21:14:50 -0800

    added keypress event listner and sounds

--- hangman/index.js
+++ hangman/index.js
@@ -1,6 +1,6 @@
 $(document).ready(()=>{
-
-    const gallow = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
+    const lose =  new Audio("sound/fail.wav")
+    const appl =  new Audio("sound/applause.wav")
     const words = ["canada", "germany", "netherlands", "belgium", "russia", "denmark", "thailand", "egypt"];
     let pic_index, tries, word, word_letters, guessed;
     // game setup
@@ -8,6 +8,7 @@ $(document).ready(()=>{
         pic_index=1;
         tries=0;
         guessed=0
+        console.log(`guessed: ${guessed}, tries: ${tries}`);
         $('.key_button').removeClass('active');
         $('.dashes').children().html("&nbsp;")
         word = words[Math.floor(Math.random()*words.length)];
@@ -30,20 +31,47 @@ $(document).ready(()=>{
                 }
             }
             index.forEach(k=>{
-                $(`.${k}`).html(word_letters[k])
+                $(`.${k}`).html(word_letters[k].toUpperCase())
+                guessed++;
             })
-            guessed++;
+            console.log(`guessed: ${guessed}, tries: ${tries}`);
         } else {
             tries++;
             $('img').attr('src', `./images/${pic_index}.jpg`)
             pic_index++;
+            console.log(`guessed: ${guessed}, tries: ${tries}`);
+        }
+        if(guessed===word_letters.length){
+            appl.play();
+            if(confirm('Congratulations! You won!')){
+              setTimeout(()=>{
+                $('.dashes').empty()
+                setUp()
+            }, 1000)  
+            }            
+        } else if (tries===6){
+            lose.play()
+            if(confirm(`You lost! The word was ${word}`)){
+              setTimeout(()=>{
+                $('.dashes').empty()
+                setUp()
+            }, 1000)  
+            }
+            
         }
     };
 
     $(".key_button").on("click", event=>{
         const {currentTarget} = event;
+        console.log(currentTarget)
         $(currentTarget).addClass("active");
         let letter = currentTarget.innerHTML.toLowerCase()
         update(letter);        
+    })
+
+    $(document).keypress(e=>{
+        let letter = e.key;
+        $(`.${letter}`).addClass("active");     
+        update(letter);
     })
 })//document ready
