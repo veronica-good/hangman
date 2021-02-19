fa47d02db7c175bd02d576f2d74d805104aa2265
Veronica <veronica.gudova@gmail.com>
2021-02-19 14:01:52 -0800

    fixed bugs, added startgame/reset option

--- hangman/index.js
+++ hangman/index.js
@@ -1,17 +1,40 @@
 $(document).ready(()=>{
     const lose =  new Audio("sound/fail.wav")
     const appl =  new Audio("sound/applause.wav")
-    const words = ["canada", "germany", "netherlands", "belgium", "russia", "denmark", "thailand", "egypt"];
-    let pic_index, tries, word, word_letters, guessed;
+    const words = [
+        "canada", 
+        "germany", 
+        "netherlands", 
+        "belgium", 
+        "russia", 
+        "denmark", 
+        "thailand", 
+        "egypt",
+        "argentina",
+        "australia",
+        "bahamas",
+        "brazil",
+        'bangladesh',
+        "china",
+        "chile",
+        "congo",
+        "ecuador",
+        "ethiopia",
+        "finland"
+    ];
+    let pic_index, tries, word, word_letters, won=0, lost=0;
+
     // game setup
     const setUp = () =>{
         pic_index=1;
         tries=0;
-        guessed=0
-        console.log(`guessed: ${guessed}, tries: ${tries}`);
+        guessed=[];
+        wrong_guess=[];
         $('.key_button').removeClass('active');
-        $('.dashes').children().html("&nbsp;")
+        $('.dashes').children().html("")
+        $('#score').html(`Wins: ${won} Loses: ${lost}`)
         word = words[Math.floor(Math.random()*words.length)];
+        $('img').attr('src', './images/0.jpg')
         console.log(word)
         word_letters=word.split('');
         if(word_letters.length>0){
@@ -20,58 +43,72 @@ $(document).ready(()=>{
             }           
         }
     }
-    setUp()
+
     const update= (letter)=>{
         let index = [];
         if(word_letters.includes(letter)){
-            for(let i=0; i<word_letters.length; i++){
+            if(!guessed.includes(letter)){
+                for(let i=0; i<word_letters.length; i++){
                 if(letter === word_letters[i]){
-                    index.push(i)
-                    // console.log(letter, index)                                          
+                    index.push(i)                                     
                 }
+                }
+                index.forEach(k=>{
+                    $(`.${k}`).html(word_letters[k].toUpperCase())
+                    guessed.push(word_letters[k]);
+                })
             }
-            index.forEach(k=>{
-                $(`.${k}`).html(word_letters[k].toUpperCase())
-                guessed++;
-            })
-            console.log(`guessed: ${guessed}, tries: ${tries}`);
-        } else {
+            
+            console.log('guessed:', guessed)
+        } else if(!wrong_guess.includes(letter)){
             tries++;
+            wrong_guess.push(letter)
+            console.log('wrong:', wrong_guess)
             $('img').attr('src', `./images/${pic_index}.jpg`)
             pic_index++;
-            console.log(`guessed: ${guessed}, tries: ${tries}`);
         }
-        if(guessed===word_letters.length){
+        if(guessed.length===word_letters.length){
             appl.play();
+            won++;
+            setTimeout(()=>{
             if(confirm('Congratulations! You won!')){
-              setTimeout(()=>{
                 $('.dashes').empty()
                 setUp()
-            }, 1000)  
             }            
+            }, 1000)  
         } else if (tries===6){
-            lose.play()
+            lose.play();
+            lost++;
+            setTimeout(()=>{
             if(confirm(`You lost! The word was ${word}`)){
-              setTimeout(()=>{
                 $('.dashes').empty()
                 setUp()
-            }, 1000)  
             }
+            }, 1000)  
             
         }
     };
 
     $(".key_button").on("click", event=>{
         const {currentTarget} = event;
-        console.log(currentTarget)
         $(currentTarget).addClass("active");
+        $(currentTarget).off();
         let letter = currentTarget.innerHTML.toLowerCase()
         update(letter);        
     })
 
-    $(document).keypress(e=>{
-        let letter = e.key;
+    $(this).keypress(event=>{
+        let letter = event.key;
         $(`.${letter}`).addClass("active");     
         update(letter);
+    })
+    
+    $('.start').click(event=>{
+        $(event.currentTarget).html("Reset game")
+        $('.dashes').empty()
+        $('#score').empty();
+        won=0;
+        lost=0;
+        setUp();
     })
 })//document ready
